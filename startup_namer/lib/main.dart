f23718e1bf5312d137c40bc784103add18c88f5b
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-07-23 10:43:05 +0200

    first commit

---
+++ startup_namer/lib/main.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: Scaffold(
+        appBar: AppBar(title: Text('WordPair Generator')),
+        body: Center(
+          child: Text('Hello World'),
+        )),
+    );
+  }
+}
